074b585dc2048658af68addd981ac3ea18def4d4
thefireholder <sangjoonlee@sangjoons-macbook-pro.local>
2017-06-18 16:36:20 -0700

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Sangjoon Lee on 6/18/17.
+//  Copyright © 2017 Sangjoon Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
