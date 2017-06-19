0220e31fd26095b03f5231b2801f74be4336252a
thefireholder <aaccjjt@hotmail.com>
2017-06-19 04:48:30 -0700

    official introduction added in main

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,9 +7,12 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    cout << "I am John Lee\n";
+    
     return 0;
 }
