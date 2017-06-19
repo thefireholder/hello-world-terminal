8359c6555c67525c8756697d10723300cfdf7000
thefireholder <aaccjjt@hotmail.com>
2017-06-19 04:55:11 -0700

    small bug fix in main about previous print statement

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     cout << "I am John Lee\n";
-    cout << "another line for xcode github commit"
+    cout << "another line for xcode github commit";
     
     return 0;
 }
